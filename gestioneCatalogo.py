05424ce57c4873fab9dc50308633e2428c415693
sergioscarale <sergio.scarale11@gmail.com>
2024-12-07 15:45:12 +0100

    Modifiche fatte in classe
    Implementazione di alcune funzioni

--- gestioneCatalogo.py
+++ gestioneCatalogo.py
@@ -41,9 +41,13 @@ def inserimentoCatalogo(catalogo):
         except ValueError:
             print("inserisci un numero di episodi valido!")
 
+    protagonista=input("Inserisci il nome del protagonista\n> ")
+
+    data_uscita=gestioneData.dataUscita()
+
     data_modifica=data_inserimento=gestioneData.dataInsMod().strftime("%Y-%m-%d")
 
-    visualizzazioni=0
+    visualizzazioni=input("Inserisci il numero di visualizzazioni!\n> ")
 
     record={
         'titolo': titolo,
@@ -51,6 +55,8 @@ def inserimentoCatalogo(catalogo):
         'tipo': tipo,
         'durata': durata,
         'episodi': episodi,
+        'protagonista': protagonista,
+        'data_uscita' : data_uscita,
         'data_inserimento': data_inserimento,
         'data_modifica': data_modifica,
         'visualizzazioni': visualizzazioni
@@ -58,24 +64,74 @@ def inserimentoCatalogo(catalogo):
     catalogo.append(record)
     print(f"Perfetto!\nHai inserito correttamente {titolo} al catalogo!")
 
-def modificaCatalogo(catalogo):
-    #rivedi
+def menuModificaCatalogo(catalogo):
     print("<--- MODIFICA CATALOGO --->\n")
-    nTitolo=input("Quale titolo si vuole modificare?").lower().capitalize()
+    print("\n<--- MENU' --->")
+    scelta=1
+    while scelta!=0:
+        print("0 - Esci")
+        print("1 - Modifica titolo;")
+        print("2 - Modifica genere;")
+        print("3 - Modifica visualizzazioni;")
+        scelta=int(input("> "))
+        match scelta:
+            case 0: break
+            case 1: modificaTitolo(catalogo)
+            case 2: modificaGenere(catalogo)
+            case 3: modificaVisualizzazioni(catalogo)
+            case __: print("numero non valido!")
+
+def modificaTitolo(catalogo):
+    print("<--- MODIFICA TITOLO --->\n")
+    titolo=input("Quale titolo si vuole modificare?\n> ").lower().capitalize()
+    nTitolo=input("Nuovo titolo > ")
     for record in catalogo:
-        for chiave in record:
-            if(chiave['titolo']==nTitolo):
-                chiave['titolo']=nTitolo
-                #meglio fare un menù o continuare così?
+        if(record['titolo']==titolo):
+            record['titolo']=nTitolo
+            print("Titolo modificato!")
+            break
+        else:
+            print("Titolo insesistente!")
+
+def modificaGenere(catalogo):
+    print("<--- MODIFICA GENERE --->\n")
+    titolo=input("Di quale titolo vuoi aggiornare il genere? ").lower().capitalize()
+    for record in catalogo:
+        if record['titolo']==titolo:
+            print(f"Generi attuali per {titolo}: {', '.join(record['genere'])}")
+            genereCatalogo={'Avventura','Azione','Commedia','Fantascienza','Fantasy','Horror','Musical','Thriller','Supereroi'}
+            nuoviGeneri=printGeneri(genereCatalogo)
+            record['genere']=nuoviGeneri 
+            print("Genere modificato con successo!")
+            break
+    else:
+        print("Titolo inesistente!")
+
+def modificaVisualizzazioni(catalogo):
+    print("<--- MODIFICA VISUALIZZAZIONI --->\n")
+    titolo=input("Di quale titolo vuoi aggiornare il protagonista? ").lower().capitalize()
+    for record in catalogo:
+        if record['titolo']==titolo:
+            print(f"Protagonista per {titolo}: {record['visualizzazioni']}")
+            try:
+                nProtagonista=input("Inserisci il nuovo protagonista\n> ")
+            except ValueError():
+                print("non valido!")
+            record['protagonista']=nProtagonista
+        else:
+            print("Titolo inesistente!")
 
 def eliminaCatalogo(catalogo):
     #rivedi
     print("<--- ELIMINA ELEMENTO DAL CATALOGO --->\n")
-    nTitolo=input("Quale titolo vuoi cancellare?").lower().capitalize()
+    titolo=input("Quale titolo vuoi cancellare?").lower().capitalize()
     for record in catalogo:
-        for chiave in record:
-            if(chiave==nTitolo):
-                record.popitem()
+        if(record['titolo']==titolo):
+                catalogo.remove(record)
+                print(f"Titolo '{titolo}' rimosso!")
+                break
+        else:
+            print("Titolo inesistente!")
 
 def printCatalogo(catalogo):
     print("\n<--- PRINT CATALOGO --->\n")
@@ -104,7 +160,7 @@ def main():
         match(scelta):
             case 0: print("ciao ciao")
             case 1: inserimentoCatalogo(catalogo)
-            case 2: modificaCatalogo(catalogo)
+            case 2: menuModificaCatalogo(catalogo)
             case 3: eliminaCatalogo(catalogo)
             case 4: printCatalogo(catalogo)
             case __: print("errore!")
--- gestioneData.py
+++ gestioneData.py
@@ -1,4 +1,10 @@
 from datetime import datetime
 def dataInsMod():
     data=datetime.now()
-    return data    
+    return data 
+
+def dataUscita():
+    data=input("Inserisci la data di uscita\n> ")
+    formato="%Y-%m-%d"
+    data_uscita=datetime.strptime(data,formato).date()
+    return data_uscita
